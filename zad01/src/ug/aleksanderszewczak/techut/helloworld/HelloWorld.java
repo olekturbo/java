a020508eae2f77ae5e8c4c42a72237ce0df58882
Aleksander Szewczak <aleksander.szewczak@inprox.pl>
2018-10-10 16:35:39 +0200

    Dodanie programu HelloWorld

---
+++ zad01/src/ug/aleksanderszewczak/techut/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package ug.aleksanderszewczak.techut.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
